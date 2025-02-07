fd9f00491d765dc378036c0fa502a560ee7b05c8
Ahmad Tayasnh <engahmadtayansa@gmail.com>
2025-02-07 19:24:02 +0200

    refactor use case in domain layer

--- lib/features/file_manager/domain/use_case/get_file.dart
+++ lib/features/file_manager/domain/use_case/get_file.dart
@@ -9,4 +9,24 @@ class GetFileUseCase {
   Future<List<FileEntity>> getFiles(String directoryPath) async {
     return await fileRepository.getFiles(directoryPath);
   }
+
+  Future<void> createFile(String directoryPath, String fileName) async {
+    return await fileRepository.createFile(directoryPath, fileName);
+  }
+
+  Future<void> deleteFile(String filePath) async {
+    return await fileRepository.deleteFile(filePath);
+  }
+  
+  Future<void> editPath(String filePath, String content) async {
+    return await fileRepository.editPath(filePath, content);
+  }
+
+  Future<List<FileEntity>> sortFiles(String directoryPath, String typeSort) async {
+    return await fileRepository.sortFiles(directoryPath, typeSort);
+  }
+  
+  Future<List<FileEntity>> filterFiles(String directoryPath, String typeFilter) async {
+    return await fileRepository.filterFiles(directoryPath, typeFilter);
+  }
 }
